207093370887cf4dfaabd0337d38f824cdebe3ed
SofiiaNikolenko <sofii.nikolenko@gmail.com>
2024-05-12 13:28:36 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,16 +3,6 @@ import { Route, Routes } from 'react-router-dom';
 
 import Layout from './Layout';
 
-// const Home = lazy(() => import('../pages/Home'));
-// const Catalog = lazy(() => import('../pages/Catalog'));
-// const Favorite = lazy(() => import('../pages/Favorites'));
-
-// <NavLink to="/">Home</NavLink>
-// <NavLink to="/publictrips">Public Trips</NavLink>
-// <NavLink to="/favouritetrips">Favorite Trips</NavLink>
-// <NavLink to="/user">User</NavLink>
-// <NavLink to="/auth">Auth</NavLink>
-
 const Home = lazy(() => import('../pages/Home'));
 const PublicTrips = lazy(() => import('../pages/PublicTrips'));
 const FavoriteTrips = lazy(() => import('../pages/FavouriteTrips'));
@@ -21,18 +11,6 @@ const Auth = lazy(() => import('../pages/Auth'));
 
 export const App = () => {
   return (
-    // <div
-    //   style={{
-    //     height: '100vh',
-    //     display: 'flex',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     fontSize: 40,
-    //     color: '#010101',
-    //   }}
-    // >
-    //   React homework template
-    // </div>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
